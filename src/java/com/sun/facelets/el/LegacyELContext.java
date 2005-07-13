545280444b1a5f2813d3f2b1635e75b81512268f
jhook <jhook>
2005-07-13 07:35:47 +0000

    added licensing terms

--- src/java/com/sun/facelets/el/LegacyELContext.java
+++ src/java/com/sun/facelets/el/LegacyELContext.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2005 Sun Microsystems, Inc. All rights reserved.
+ * Licensed under the Common Development and Distribution License,
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.sun.com/cddl/
+ *   
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package com.sun.facelets.el;
 
 import java.lang.reflect.Method;
