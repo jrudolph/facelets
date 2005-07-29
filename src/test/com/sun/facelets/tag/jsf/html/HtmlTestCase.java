8753026f5c555a441a88d5f6fdc0e71299fcb1f7
jhook <jhook>
2005-07-29 03:03:17 +0000

    added test case for fixing TCCI

--- src/test/com/sun/facelets/tag/jsf/html/HtmlTestCase.java
+++ src/test/com/sun/facelets/tag/jsf/html/HtmlTestCase.java
@@ -1,11 +1,22 @@
 package com.sun.facelets.tag.jsf.html;
 
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+
+import com.sun.facelets.Facelet;
+import com.sun.facelets.FaceletFactory;
 import com.sun.facelets.FaceletTestCase;
 
 public class HtmlTestCase extends FaceletTestCase {
 
     public void testPanelGrid() throws Exception {
-        
+        FacesContext faces = FacesContext.getCurrentInstance();
+
+        FaceletFactory f = FaceletFactory.getInstance();
+        Facelet at = f.getFacelet("panelGrid.xml");
+
+        UIViewRoot root = faces.getViewRoot();
+        at.apply(faces, root);
     }
 
 }
