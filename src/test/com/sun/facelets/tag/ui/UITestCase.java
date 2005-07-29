3ee59f2b7b8002238899868a316c49c34d0ed352
jhook <jhook>
2005-07-29 00:30:02 +0000

    added a test case for ui:component

--- src/test/com/sun/facelets/tag/ui/UITestCase.java
+++ src/test/com/sun/facelets/tag/ui/UITestCase.java
@@ -1,11 +1,15 @@
 package com.sun.facelets.tag.ui;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
 
 import com.sun.facelets.Facelet;
 import com.sun.facelets.FaceletFactory;
 import com.sun.facelets.FaceletTestCase;
+import com.sun.faces.util.DebugUtil;
 
 public class UITestCase extends FaceletTestCase {
 
@@ -18,7 +22,22 @@ public class UITestCase extends FaceletTestCase {
         UIViewRoot root = faces.getViewRoot();
         at.apply(faces, root);
         
+    }
+    
+    public void testComponent() throws Exception {
+        FacesContext faces = FacesContext.getCurrentInstance();
+        Map map = new HashMap();
+        faces.getExternalContext().getRequestMap().put("map", map);
+
+        FaceletFactory f = FaceletFactory.getInstance();
+        Facelet at = f.getFacelet("component.xml");
+
+        UIViewRoot root = faces.getViewRoot();
+        at.apply(faces, root);
         
+        assertEquals("only one child, the component", 1, root.getChildCount());
+        assertNotNull("bound to map", map.get("c"));
+        DebugUtil.printTree(root, System.out);
     }
 
 }
