901696f6b36aa7ebdd255694a7e704e932bad9c6
jhook <jhook>
2005-09-23 06:17:54 +0000

    added portlet demo

---
+++ demo/portlet/README.txt
@@ -0,0 +1,5 @@
+Created by Nicolas Belisle
+
+This is a very small facelets+myfaces portlet example.  It's been tested on Jetsepeed 2 and Liferay portals.  It supports the edit and help modes.
+
+If you have questions or comments, go to http://facelets.dev.java.net and post your questions to the user list.
---
+++ demo/portlet/src/com/sun/facelets/FaceletPortletViewHandler.java
@@ -0,0 +1,54 @@
+package com.sun.facelets;
+
+import java.io.IOException;
+
+import javax.faces.FacesException;
+import javax.faces.application.ViewHandler;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.render.RenderKit;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * ViewHandler implementation for Facelets used in Portlets
+ * 
+ */
+public class FaceletPortletViewHandler extends FaceletViewHandler {
+
+    public FaceletPortletViewHandler(ViewHandler parent) {
+        super(parent);
+    }
+
+    protected ResponseWriter createResponseWriter(FacesContext context)
+            throws IOException, FacesException {
+        ExternalContext extContext = context.getExternalContext();
+        RenderKit renderKit = context.getRenderKit();
+
+        RenderRequest request = (RenderRequest) extContext.getRequest();
+        RenderResponse response = (RenderResponse) extContext.getResponse();
+
+        String contenttype = request.getResponseContentType();
+        if (contenttype == null) {
+            contenttype = "text/html";
+        }
+
+        String encoding = response.getCharacterEncoding();
+        if (encoding == null) {
+            encoding = "ISO-8859-1";
+        }
+
+        ResponseWriter writer = renderKit.createResponseWriter(NullWriter.Instance, contenttype, encoding);
+
+        contenttype = writer.getContentType();
+
+        // apply them to the response
+        response.setContentType(contenttype);
+
+        // Now, clone with the real writer
+        writer = writer.cloneWithWriter(response.getWriter());
+
+        return writer;
+    }
+}
