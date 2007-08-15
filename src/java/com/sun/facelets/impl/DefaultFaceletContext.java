c70d245447cc7494a8edaf55c1f37ddcedb01bdf
youngm <youngm>
2007-08-15 21:09:20 +0000

    Issue number:  234 Rolled back the removal of the 2 lines of code until Jacob can get a chance to look at this issue.

--- src/java/com/sun/facelets/impl/DefaultFaceletContext.java
+++ src/java/com/sun/facelets/impl/DefaultFaceletContext.java
@@ -273,10 +273,12 @@ final class DefaultFaceletContext extends FaceletContext {
         boolean found = false;
         TemplateManager client;
 
-        for (int i = 0, size = this.clients.size(); i < size && !found; i++) {
+        for (int i = 0; i < this.clients.size() && found == false; i++) {
             client = ((TemplateManager) this.clients.get(i));
             if (client.equals(this.facelet))
                 continue;            
+            if (client.isRoot() && i != 0)
+            	break;
             found = client.apply(this, parent, name);            
         }
 
