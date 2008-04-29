ed1de92ba5cc516a339c0de8bf8719b74aa41cbf
edburns <edburns>
2008-04-29 19:58:27 +0000

    snapshot

---
+++ src/java/com/sun/facelets/tag/renderkit/TemplateRendererTagHandler.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sun.facelets.tag.renderkit;
+
+import com.sun.facelets.tag.jsf.ComponentConfig;
+import com.sun.facelets.tag.jsf.ComponentHandler;
+
+/**
+ *
+ * @author edburns
+ */
+public class TemplateRendererTagHandler extends ComponentHandler {
+    
+    TemplateRendererTagHandler(ComponentConfig config) {
+        super(config);
+    }
+    
+}
