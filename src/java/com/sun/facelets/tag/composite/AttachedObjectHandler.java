3b773e4f7daf5c2a73fbb8e8446a3fd628c302c8
edburns <edburns>
2008-07-02 20:34:33 +0000

    checkpoint

---
+++ src/java/com/sun/facelets/tag/composite/AttachedObjectHandler.java
@@ -0,0 +1,79 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sun.facelets.tag.composite;
+
+import com.sun.facelets.FaceletContext;
+import com.sun.facelets.FaceletException;
+import com.sun.facelets.tag.TagAttribute;
+import com.sun.facelets.tag.TagConfig;
+import com.sun.facelets.tag.TagException;
+import com.sun.facelets.tag.TagHandler;
+import java.beans.BeanDescriptor;
+import java.beans.BeanInfo;
+import java.io.IOException;
+import java.util.List;
+import javax.el.ELException;
+import javax.el.ValueExpression;
+import javax.faces.FacesException;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.webapp.pdl.AttachedObjectTarget;
+
+/**
+ *
+ * @author edburns
+ */
+public class AttachedObjectHandler extends TagHandler {
+    
+    private TagAttribute name = null;
+    private TagAttribute targets = null;
+
+    public AttachedObjectHandler(TagConfig config) {
+        super(config);
+        this.name = this.getRequiredAttribute("name");
+        this.targets = this.getAttribute("targets");
+        
+    }
+    
+    public void apply(FaceletContext ctx, UIComponent parent) throws IOException, FacesException, FaceletException, ELException {
+
+        BeanInfo componentBeanInfo = (BeanInfo)
+                parent.getAttributes().get(UIComponent.BEANINFO_KEY);
+        if (null == componentBeanInfo) {
+            throw new TagException(this.tag, "Error: I have an EditableValueHolder tag, but no enclosing composite component");
+        }
+        BeanDescriptor componentDescriptor = componentBeanInfo.getBeanDescriptor();
+        if (null == componentDescriptor) {
+            throw new TagException(this.tag, "Error: I have an EditableValueHolder tag, but no enclosing composite component");
+        }
+
+        FacesContext context = ctx.getFacesContext();
+        List<AttachedObjectTarget> targetList = (List<AttachedObjectTarget>)
+                componentDescriptor.getValue(AttachedObjectTarget.ATTACHED_OBJECT_TARGETS_KEY);
+        AttachedObjectTargetImpl target = new AttachedObjectTargetImpl();
+        targetList.add(target);
+        target.setComponent(parent);
+        
+        ValueExpression ve = null;
+        String strValue = null;
+
+        ve = name.getValueExpression(ctx, String.class);
+        strValue = (String) ve.getValue(ctx);
+        if (null != strValue) {
+            target.setName(strValue);
+        }
+
+        if (null != targets) {
+            ve = targets.getValueExpression(ctx, String.class);
+            strValue = (String) ve.getValue(ctx);
+            if (null != strValue) {
+                target.setTargetsList(strValue);
+            }
+        }
+        
+    }
+
+}
